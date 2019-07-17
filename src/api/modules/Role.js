ef2211291a71a43b8d08fe21242f35aa06cd9786
moz <nn880328@126.com>
2019-07-17 08:29:00 +0800

    增加角色菜单功能

--- src/api/modules/Role.js
+++ src/api/modules/Role.js
@@ -5,11 +5,14 @@ const add = (formData) => axios.post('/api/role/add', formData);
 const remove = (formData) => axios.post('/api/role/delete', formData);
 const update = (formData) => axios.post('/api/role/update', formData);
 const loadConfig = (formData) => axios.get('/api/role/config', {params: formData});
-
+const putMenu = (formData) => axios.put('/api/role/menu', formData);
+const deleteMenu = (formData) => axios.delete('/api/role/menu', {params: formData});
 export default {
     load,
     add,
     remove,
     update,
     loadConfig,
+    putMenu,
+    deleteMenu,
 }
--- src/views/Auth/Role-Config.vue
+++ src/views/Auth/Role-Config.vue
@@ -6,13 +6,13 @@
                     <i class="el-icon-collection-tag"></i>
                     {{item.name}}
                 </div>
-                <el-switch @change="parentHandle(item.checked,item)" v-model="item.checked"></el-switch>
+                <el-switch @change="parentHandle(item)" v-model="item.checked"></el-switch>
             </div>
             <el-row :gutter="40">
                 <el-col v-for="element in item.children">
                     <div class="menu-box am-padding-vertical-xs">
                         <div class="am-text-xs">{{element.name}}</div>
-                        <el-switch @change="childHandle(element.checked,item)" v-model="element.checked"></el-switch>
+                        <el-switch @change="childHandle(element,item)" v-model="element.checked"></el-switch>
                     </div>
                 </el-col>
             </el-row>
@@ -32,35 +32,71 @@
             };
         },
         created() {
-            this.loadRoleConfig(this.id);
+            this.loadRoleConfig();
+        },
+        watch: {
+            // 如果路由有变化，会再次执行该方法
+            '$route': 'loadRoleConfig'
         },
         methods: {
-            async loadRoleConfig(id) {
-                let {status, data} = await Role.loadConfig({id});
+            async loadRoleConfig() {
+                let {status, data} = await Role.loadConfig({id: this.id});
                 if (status) {
                     this.menu = data;
                 }
             },
             /*
             * 父级菜单switch
-            * status    switch状态
-            * children      子级菜单集合
+            * nodeData      父级菜单
             * */
-            parentHandle(status, {children}) {
+            async parentHandle({checked, children, id}) {
+                //修改父级菜单
+                if (checked) {
+                    let {status} = await Role.putMenu({role_id: this.id, menu_id: id});
+                } else {
+                    let {status} = await Role.deleteMenu({role_id: this.id, menu_id: id});
+                }
                 //所有子级跟随父级状态改变
-                children.forEach(function (item) {
-                    item.checked = status;
+                children.forEach(async (item) => {
+                    item.checked = checked;
+                    if (checked) {
+                        let {status} = await Role.putMenu({role_id: this.id, menu_id: item.id});
+                    } else {
+                        let {status} = await Role.deleteMenu({role_id: this.id, menu_id: item.id});
+                    }
                 });
             },
             /*
             * 子级菜单switch,子级菜单有一个选中，父级选中
-            * status    switch状态
-            * parentData      父级data
+            * nodeData    子级data
+            * parentData  父级data
             * */
-            childHandle(status, parentData) {
+            async childHandle({checked, id}, parentData) {
                 //检测子级菜单中是否有选中状态
-                let flag = parentData.children.some(item => item.checked);
-                parentData.checked = flag;
+                let num = parentData.children.reduce((total, item) => {
+                    if (item.checked) {
+                        total++;
+                    }
+                    return total;
+                }, 0);
+                switch (num) {
+                    case 0:
+                        await Role.deleteMenu({role_id: this.id, menu_id: parentData.id});
+                        parentData.checked = false;
+                        break;
+                    case 1:
+                        await Role.putMenu({role_id: this.id, menu_id: parentData.id});
+                        parentData.checked = true;
+                        break;
+                    default:
+                        break;
+                }
+                if (checked) {
+                    let {status} = await Role.putMenu({role_id: this.id, menu_id: id});
+                } else {
+                    let {status} = await Role.deleteMenu({role_id: this.id, menu_id: id});
+                }
+
             },
         },
     }
