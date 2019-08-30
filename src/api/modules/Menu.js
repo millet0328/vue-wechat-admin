d20e60e4d81aa5a4e9884db7f67dd975158dbeb6
moz <nn880328@126.com>
2019-08-30 08:19:28 +0800

    更新api接口地址

--- src/api/modules/Menu.js
+++ src/api/modules/Menu.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
 const loadSub = (formData) => axios.get('/api/menu/sub', {params: formData});
-const add = (formData) => axios.post('/api/menu/add', formData);
-const update = (formData) => axios.post('/api/menu/update', formData);
-const remove = (formData) => axios.post('/api/menu/delete', formData);
+const add = (formData) => axios.post('/api/menu', formData);
+const update = (formData) => axios.put('/api/menu', formData);
+const remove = (formData) => axios.delete('/api/menu', {params: formData});
 const loadTree = (formData) => axios.get('/api/menu/tree', {params: formData});
 export default {
     loadSub,
--- src/api/modules/Role.js
+++ src/api/modules/Role.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
 const load = (formData) => axios.get('/api/role/list', {params: formData});
-const add = (formData) => axios.post('/api/role/add', formData);
-const remove = (formData) => axios.post('/api/role/delete', formData);
-const update = (formData) => axios.post('/api/role/update', formData);
+const add = (formData) => axios.post('/api/role', formData);
+const remove = (formData) => axios.delete('/api/role', {params: formData});
+const update = (formData) => axios.put('/api/role', formData);
 const loadConfig = (formData) => axios.get('/api/role/config', {params: formData});
 const putMenu = (formData) => axios.put('/api/role/menu', formData);
 const deleteMenu = (formData) => axios.delete('/api/role/menu', {params: formData});
